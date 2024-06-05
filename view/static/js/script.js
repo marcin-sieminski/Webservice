e438ccaa5705b304eec58f15ca2117440fa771d8
Marcin Siemiński <marcin.sieminski@outlook.com>
2024-06-05 08:03:01 +0200

    Minor changes

--- view/static/js/script.js
+++ view/static/js/script.js
@@ -1,9 +1,16 @@
-function deleteItem(){        
-    const itemId = parseInt(document.getElementById("itemId").textContent)
-    
-    console.log(itemId)
+async function deleteItem(){        
+    const itemId = parseInt(document.querySelector(".delete-confirmation").getAttribute("data-item-id"))
+    const response = await fetch(`/item/delete?id=${itemId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: itemId
+          })
+    })
 }
 
 function home(){
- 
+    location.href = '/';    
 }
