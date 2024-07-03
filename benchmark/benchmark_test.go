2504f049c669d6c83e54af728a140d0bb7f0a8a1
Marcin Siemiński <marcin.sieminski@outlook.com>
2024-07-03 22:02:06 +0200

    Improve benchmark

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -1,17 +1,31 @@
 package benchmark
 
 import (
+	"flag"
 	"testing"
+
+	"github.com/marcin-sieminski/webservice/model"
 )
 
-func init() {
+var (
+	endpoint = flag.String("endpoint", "http://localhost:4000/v1/items", "Endpoint for the items list web service")
+)
 
+type application struct {
+	itemslist *model.ItemslistModel
 }
 
 func BenchmarkApi(b *testing.B) {
+	b.StopTimer()
+	app := &application{
+		itemslist: &model.ItemslistModel{Endpoint: *endpoint},
+	}
+
 	for i := 0; i < b.N; i++ {
-		var err = benchmarkApi()
-		if err {
+		b.StartTimer()
+		_, err := app.itemslist.GetAll()
+		b.StopTimer()
+		if err != nil {
 			b.Fatal("api error")
 		}
 	}
